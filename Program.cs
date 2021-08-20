b267881dd5484a57c68f0cf4c9a5ba144be8ddc9
Samantha Sishi <samanthasishi08@gmail.com>
2021-08-20 08:55:21 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pracFriday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
