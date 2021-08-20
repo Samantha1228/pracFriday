d649ad2758f2cb702c5abcc7a2fa741139234ed5
Samantha Sishi <samanthasishi08@gmail.com>
2021-08-20 09:36:20 +0200

    changes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace pracFriday
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("aw yebo");
         }
     }
 }
